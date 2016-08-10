58d3fdb0bb4a8cc68c8b3d8ba507f34edcbeed6e
ksanjayreddy <ksanjayreddy817@gmail.com>
2016-08-10 11:45:34 +0530

    login added

--- app/src/main/java/com/example/android/cetscoreboard/MainActivity.java
+++ app/src/main/java/com/example/android/cetscoreboard/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.android.cetscoreboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();  // to hide the action bar
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // UPDATER BUTTON IS LINKED TO LOGIN ACTIVITY
+        Button b1 = (Button) findViewById(R.id.updater_button_id);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i1);
+            }
+        });
     }
 }
